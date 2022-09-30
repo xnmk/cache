f90dfbd1da3cbb71723507e75e62b2e81145d58b
xnmk_zhan <xnmk_zhan@163.com>
2022-09-30 17:34:40 +0800

    Initial commit

---
+++ cache-api/src/main/java/com/github/xnmk/cache/api/ICache.java
@@ -0,0 +1,11 @@
+package com.github.xnmk.cache.api;
+
+import java.util.Map;
+
+/**
+ * 缓存接口
+ * @author:xnmk
+ * @Since:0.0.1
+ */
+public interface ICache<K, V> extends Map<K, V> {
+}
---
+++ cache-core/src/main/java/com/github/xnmk/cache/core/Cache.java
@@ -0,0 +1,10 @@
+package com.github.xnmk.cache.core;
+
+import com.github.xnmk.cache.api.*;
+
+/**
+ * @author:xnmk
+ * @Since:0.0.1
+ */
+public class Cache implements {
+}
